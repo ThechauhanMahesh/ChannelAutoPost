96914bb853121f886fbd36f835ca48850f1ae36f
ThechauhanMahesh <125787905+ThechauhanMahesh@users.noreply.github.com>
2023-05-28 20:53:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,11 +29,8 @@ try:
     bottoken = "6236436412:AAFKXycXizxeZiUNZ_VWG9qDE0gmPEdP2DY"
     frm = [int("-1001758866038")] 
     tochnls = [int("-1001347758829")]
-    tochnls2 = [int("-100178630988")]
-    frm2 = [int("-1001665024195")]
+    tochnls2 = [int("-100178630988"), int("-1001665024195")]
     datgbot = TelegramClient("bot", apiid, apihash).start(bot_token=bottoken)
-    client = Client("cap", bot_token=bottoken, api_id=int(apiid), api_hash=apihash)    
-    client.start()
 except:
     logging.error("Environment vars are missing! Kindly recheck.")
     logging.info("Bot is quiting...")
@@ -42,28 +39,42 @@ except:
 
 @datgbot.on(events.NewMessage(incoming=True,func=lambda e: e.is_private))
 async def _(event):
-    tochnl = [tochnls2[0], frm2[0]]
-    if str(event.text).lower() == "/start":
-        return
-    if event.photo:
-        photo = event.media.photo
-        await datgbot.send_file(tochnl, photo, caption=event.text, link_preview=False)
-    elif event.media:
+    for tochnl in tochnls2:
         try:
-            if event.media.webpage:
-                await datgbot.send_message(tochnl, event.text, link_preview=False)           
-        except Exception:
-            media = event.media.document
-            await datgbot.send_file(tochnl, media, caption=event.text, link_preview=False)
-         
-    else:
-        await datgbot.send_message(tochnl, event.text, link_preview=False)
-        
+            if event.poll:
+                return
+            if event.photo:
+                photo = event.media.photo
+                await datgbot.send_file(
+                    tochnl, photo, caption=event.text, link_preview=False
+                )
+            elif event.media:
+                try:
+                    if event.media.webpage:
+                        await datgbot.send_message(
+                            tochnl, event.text, link_preview=False
+                        )
+                except Exception:
+                    media = event.media.document
+                    await datgbot.send_file(
+                        tochnl, media, caption=event.text, link_preview=False
+                    )
+                finally:
+                    return
+            else:
+                await datgbot.send_message(tochnl, event.text, link_preview=False)
+        except Exception as exc:
+            logging.error(
+                "TO_CHANNEL ID is wrong or I can't send messages there (make me admin).\nTraceback:\n%s",
+                exc,
+            )
+
+
                         
-@datgbot.on(events.NewMessage(incoming=True, chats=frm2))
+@datgbot.on(events.NewMessage(incoming=True, pattern="/start"))
 async def __(event):
-    await client.copy_message(tochnls2[0], frm2[0], event.id)
-        
+    await event.reply("@DroneBots")
+    
 @datgbot.on(events.NewMessage(incoming=True, chats=frm))
 async def _(event):
     for tochnl in tochnls:
