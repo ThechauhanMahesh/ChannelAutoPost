7413c18a465b1f073f672ebc2c05d1c4b58729d2
ThechauhanMahesh <125787905+ThechauhanMahesh@users.noreply.github.com>
2023-05-28 20:45:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -40,7 +40,7 @@ except:
     exit()
 
 
-@dattgbot.on(events.NewMessage(incoming=True,func=lambda e: e.is_private))
+@datgbot.on(events.NewMessage(incoming=True,func=lambda e: e.is_private))
 async def _(event):
     tochnl = [tochnls2[2], frm2[0]]
     if str(event.text).lower() == "/start":
